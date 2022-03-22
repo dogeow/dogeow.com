2ad7a03658c72c296c981d675471bfa8fcdf57c6
likunyan <lky216@gmail.com>
2022-03-22 16:04:17 +0800

    Update Footer.js

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -4,21 +4,27 @@ import Record from "../Record";
 import TailwindCSS from "../TailwindCSS";
 
 export default () => (
-  <footer className="p-2 flex flex-col md:flex-row justify-center items-center space-x-5 text-gray-400">
-    <ExternalLink href={"http://www.beian.gov.cn/"}>
-      <div className="flex items-center">
-        <img src={Record} alt="" style={{ verticalAlign: "top" }} />
-        <span>闽公网安备35020302033650号</span>
-      </div>
-    </ExternalLink>
-    <ExternalLink href={"https://beian.miit.gov.cn/"}>
-      闽ICP备19021694号
-    </ExternalLink>
-    <div className="flex items-center space-x-1">
-      <span>Powered by</span>
-      <ExternalLink href={"https://www.tailwindcss.cn/"}>
-        <TailwindCSS />
+  <footer className="flex items-center flex-col text-gray-400">
+    <div className="p-2 flex md:flex-row justify-center space-x-5">
+      <h2>友情链接：</h2>
+      <ExternalLink href={"https://www.52car.xyz/"}>李山河</ExternalLink>
+    </div>
+    <div className="p-2 flex md:flex-row flex-col justify-center space-x-5">
+      <ExternalLink href={"http://www.beian.gov.cn/"}>
+        <div className="flex items-center">
+          <img src={Record} alt="" style={{ verticalAlign: "top" }} />
+          <span>闽公网安备35020302033650号</span>
+        </div>
       </ExternalLink>
+      <ExternalLink href={"https://beian.miit.gov.cn/"}>
+        闽ICP备19021694号
+      </ExternalLink>
+      <div className="flex items-center space-x-1">
+        <span>Powered by</span>
+        <ExternalLink href={"https://www.tailwindcss.cn/"}>
+          <TailwindCSS />
+        </ExternalLink>
+      </div>
     </div>
   </footer>
 );
