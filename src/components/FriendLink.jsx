7a46907504f21af834f0933cb1abb0d9be71ea84
likunyan <lky216@gmail.com>
2022-05-09 17:58:18 +0800

    component

---
+++ src/components/FriendLink.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ExternalLink from "../components/ExternalLink";
+
+export default () => (
+  <div className="p-2 flex md:flex-row justify-center space-x-5">
+    <h2>友情链接：</h2>
+    <ExternalLink href={"https://www.52car.xyz/"}>李山河</ExternalLink>
+  </div>
+);
--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -2,13 +2,11 @@ import React from "react";
 import ExternalLink from "../components/ExternalLink";
 import Record from "../Record";
 import TailwindCSS from "../TailwindCSS";
+import FriendLink from "../components/FriendLink";
 
 export default () => (
   <footer className="flex items-center flex-col text-gray-400">
-    <div className="p-2 flex md:flex-row justify-center space-x-5">
-      <h2>友情链接：</h2>
-      <ExternalLink href={"https://www.52car.xyz/"}>李山河</ExternalLink>
-    </div>
+    <FriendLink />
     <div className="p-2 flex md:flex-row flex-col justify-center space-x-5">
       <ExternalLink href={"http://www.beian.gov.cn/"}>
         <div className="flex items-center">
