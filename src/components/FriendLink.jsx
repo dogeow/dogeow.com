22f3df01702054013474c51034d6c48107eda940
likunyan <lky216@gmail.com>
2022-05-09 18:12:11 +0800

    pure

--- src/components/FriendLink.jsx
+++ src/components/FriendLink.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import ExternalLink from "../components/ExternalLink";
 
 export default () => (
-  <div className="p-2 flex md:flex-row justify-center space-x-5">
+  <>
     <h2>友情链接：</h2>
     <ExternalLink href={"https://www.52car.xyz/"}>李山河</ExternalLink>
-  </div>
+  </>
 );
