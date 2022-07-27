e1e6cb6af199fc630511c7837af8e0e8ae64a37e
DogeOW <lky216@gmail.com>
2022-07-27 11:58:12 +0800

    Update ExternalLink.jsx

--- src/components/ExternalLink.jsx
+++ src/components/ExternalLink.jsx
@@ -1,5 +1,13 @@
 import React from "react";
 
+/**
+ * 外部链接
+ *
+ * @param {object} props
+ * @param {string} props.href 链接地址
+ * @param {JSX.Element} props.children 子节点
+ * @return {JSX.Element}
+ */
 export default function (props) {
   return (
     <a className="block" href={props.href} target="_blank" rel="noreferrer">
