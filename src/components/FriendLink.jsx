782c87ab05bbd4589c8d9cddf64aa70f28509e2d
example <example@example.com>
2022-12-22 23:43:22 +0800

    Update FriendLink.jsx

--- src/components/FriendLink.jsx
+++ src/components/FriendLink.jsx
@@ -4,13 +4,13 @@ import ExternalLink from "../components/ExternalLink";
 export default () => (
   <div className="flex items-center space-x-2">
     <h2>友情链接：</h2>
-    <ExternalLink href={"https://www.52car.xyz/"} referrer={true}>
+    <ExternalLink href={"https://www.52car.xyz"} referrer={true}>
       李山河
     </ExternalLink>
-    <ExternalLink href={"http://xiaocai.streamcc.top/"} referrer={true}>
+    <ExternalLink href={"http://xiaocai.streamcc.top"} referrer={true}>
       小菜博客
     </ExternalLink>
-    <ExternalLink href={"https://www.supereggs.cn/"} referrer={true}>
+    <ExternalLink href={"https://www.supereggs.cn"} referrer={true}>
       SuperEggs
     </ExternalLink>
   </div>
