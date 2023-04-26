c321ce58077ce696782578d11e9f314f43f9cbe9
likunyan <lky216@gmail.com>
2023-04-26 23:41:04 +0800

    Update FriendLink.jsx

--- src/components/FriendLink.jsx
+++ src/components/FriendLink.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import ExternalLink from "../components/ExternalLink";
 
-export default (props) => (
-  <div className="flex items-center space-x-2">
-    <h2>友情链接：</h2>
-    {props.links.length &&
-      props.links.map((link, index) => (
-        <ExternalLink href={link.url} referrer={true} key={index}>
-          {link.title}
-        </ExternalLink>
-      ))}
-  </div>
-);
+const FriendLinks = ({ links = [] }) => {
+  const linkItems = links.map((link) => (
+    <ExternalLink href={link.url} key={link.id} referrer="no-referrer">
+      {link.title}
+    </ExternalLink>
+  ));
+
+  return links.length ? (
+    <div className="flex items-center space-x-2">
+      <h2>友情链接：</h2>
+      {linkItems}
+    </div>
+  ) : null;
+};
+
+export default FriendLinks;
