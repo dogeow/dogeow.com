c8e02dfdd8334bbf1812963f436b0d1c9f58df83
likunyan <lky216@gmail.com>
2023-05-25 14:30:00 +0800

    Update Main.js

--- src/layouts/Main.js
+++ src/layouts/Main.js
@@ -1,36 +1,55 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import projects from "../resources/projects.json";
 
-const DOING_AND_DOT_MAX_LENGTH = 4; // doing 加上小数点额最大长度（doing 为一个单位），比如 「正在学习 Golang...」，那么长度为 4，其中小数点最长为 3 位。
+const DOING_WITH_DOT_MAX_LENGTH = 4; // doing 带小数点后的最大长度（doing 为一个单位），比如 「正在学习 Golang...」，那么长度为 4，其中小数点最长为 3 位。
 
-export default function Main(props) {
+// 正在做什么事情的文本
+const getDoingText = (doings, timestamp) => {
+  // 填充后的最大索引
+  const doingWithDotMaxIndex = doings.length * DOING_WITH_DOT_MAX_LENGTH - 1;
+  // 当前到第几个 doing 和第几个小数点
+  const doingAndDotIndex = timestamp % (doingWithDotMaxIndex + 1);
+  // 当前到第几个 doing
+  const doingIndex = Math.floor(doingAndDotIndex / DOING_WITH_DOT_MAX_LENGTH);
+  // 第几个小数点
+  const dotCount = doingAndDotIndex % DOING_WITH_DOT_MAX_LENGTH;
+
+  return `正在${doings[doingIndex]}.${".".repeat(dotCount)}`;
+};
+
+export default function Main({ doings }) {
   const [doing, setDoing] = useState("");
 
+  const projectElements = useMemo(
+    () =>
+      projects.map((project) => (
+        <a
+          className="block p-4 hover:bg-white hover:bg-opacity-20 cursor-pointer"
+          href={project.link}
+          key={project.id}
+          rel="noopener noreferrer"
+        >
+          <div className="flex flex-col items-center space-y-4">
+            <img width="40" src={project.image} alt={project.title} />
+            <div>{project.title}</div>
+          </div>
+        </a>
+      )),
+    []
+  );
+
   useEffect(() => {
-    if (!props.doings || props.doings.length === 0) {
+    if (!doings || doings.length === 0) {
       return;
     }
 
     const intervalId = setInterval(() => {
       const timestamp = Math.floor(Date.now() / 1000);
-
-      const doingMaxIndex = props.doings.length - 1;
-      const doingAndDotMaxIndex =
-        (doingMaxIndex + 1) * DOING_AND_DOT_MAX_LENGTH - 1;
-      const doingAndDotIndex = timestamp % (doingAndDotMaxIndex + 1);
-      const doingIndex = Math.floor(
-        doingAndDotIndex / DOING_AND_DOT_MAX_LENGTH
-      );
-
-      const dotCount = doingAndDotIndex % DOING_AND_DOT_MAX_LENGTH;
-      const dotString = ".".repeat(dotCount);
-
-      const doingText = `正在${props.doings[doingIndex]}${dotString}`;
-      setDoing(doingText);
+      setDoing(getDoingText(doings, timestamp));
     }, 1000);
 
     return () => clearInterval(intervalId);
-  }, [props.doings]);
+  }, [doings]);
 
   return (
     <main className="mx-auto space-y-8">
@@ -38,19 +57,7 @@ export default function Main(props) {
         <span role="img">👏</span>欢迎<span role="img">👏</span>
       </div>
       <nav className="grid gap-2 grid-cols-2 md:grid-cols-4">
-        {projects.map((project) => (
-          <a
-            className="block p-4 hover:bg-white hover:bg-opacity-20 cursor-pointer"
-            href={project.link}
-            key={project.id}
-            rel="noopener noreferrer"
-          >
-            <div className="flex flex-col items-center space-y-4">
-              <img width="40" src={project.image} alt={project.title} />
-              <div>{project.title}</div>
-            </div>
-          </a>
-        ))}
+        {projectElements}
       </nav>
       <div className="flex justify-center text">
         <a
